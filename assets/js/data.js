5e2bb7e6e4811976be452aed41a609304976ca5d
altdunya <altdunyadan@gmail.com>
2026-03-07 00:04:29 +0300

    Add files via upload

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,25 +1,48 @@
-
 window.ALT_ITEMS = [
   {
-    id: "ob-7735",
-    type: "openbor",
-    title: "OpenBOR – AltDünya Pack v1.0",
-    desc: "Kurulum rehberi + Archive.org indirme butonu. İlk başlangıç paketi.",
-    tags: ["openbor","beat'em up","engine"],
-    url: "openbor/openbor-7735.html",
-    added: "2026-02-28",
-    version: "v1.0",
-    thumb: "assets/img/thumb-openbor.svg"
+    "id": "free-abuse",
+    "type": "free",
+    "title": "Abuse (1996)",
+    "desc": "Kült platform‑shooter klasiği. AltDünya Pack v1.0 ile tek tık çalışır.",
+    "tags": [
+      "freeware",
+      "retro",
+      "dos",
+      "kurulum"
+    ],
+    "url": "free/abuse.html",
+    "added": "2026-03-07",
+    "version": "v1.0",
+    "thumb": "assets/img/thumb-free-a.svg"
   },
   {
-    id: "ob-heman",
-    type: "openbor",
-    title: "He-Man: Masters of the Universe",
-    desc: "OpenBOR tabanlı fan yapımı beat'em up. AltDünya Pack v1.0.",
-    tags: ["openbor","he-man","beat'em up"],
-    url: "openbor/he-man.html",
-    added: "2026-03-06",
-    version: "v1.0",
-    thumb: "assets/img/thumb-openbor.svg"
+    "id": "ob-heman",
+    "type": "openbor",
+    "title": "He-Man: Masters of the Universe",
+    "desc": "OpenBOR tabanlı fan yapımı beat'em up. AltDünya Pack v1.0.",
+    "tags": [
+      "openbor",
+      "heman",
+      "beatemup"
+    ],
+    "url": "openbor/he-man.html",
+    "added": "2026-03-07",
+    "version": "v1.0",
+    "thumb": "assets/img/thumb-openbor.svg"
+  },
+  {
+    "id": "ob-7735",
+    "type": "openbor",
+    "title": "OpenBOR – AltDünya Pack v1.0",
+    "desc": "Kurulum rehberi + Archive.org indirme butonu. İlk başlangıç paketi.",
+    "tags": [
+      "openbor",
+      "engine",
+      "beatemup"
+    ],
+    "url": "openbor/openbor-7735.html",
+    "added": "2026-02-28",
+    "version": "v1.0",
+    "thumb": "assets/img/thumb-openbor.svg"
   }
 ];
