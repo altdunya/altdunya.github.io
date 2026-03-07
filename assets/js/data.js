758e964fb818c9b583578b75eab7ad4b720072c3
altdunya <altdunyadan@gmail.com>
2026-03-08 00:06:14 +0300

    Add files via upload

--- assets/js/data.js
+++ assets/js/data.js
@@ -4,12 +4,7 @@ window.ALT_ITEMS = [
     "type": "free",
     "title": "Abuse (1996)",
     "desc": "Karanlık atmosferi, hızlı oynanışı ve farklı kontrol yapısıyla öne çıkan kült aksiyon klasiği. AltDünya Pack v1.0 ile tek tık çalışır.",
-    "tags": [
-      "freeware",
-      "retro",
-      "dos",
-      "aksiyon"
-    ],
+    "tags": ["freeware", "retro", "dos", "aksiyon"],
     "url": "free/abuse.html",
     "added": "2026-03-07",
     "version": "v1.0",
@@ -20,26 +15,18 @@ window.ALT_ITEMS = [
     "type": "openbor",
     "title": "He-Man: Masters of the Universe",
     "desc": "OpenBOR tabanlı fan yapımı beat'em up. AltDünya Pack v1.0.",
-    "tags": [
-      "openbor",
-      "heman",
-      "beatemup"
-    ],
+    "tags": ["openbor", "heman", "beatemup", "motu"],
     "url": "openbor/he-man.html",
     "added": "2026-03-07",
     "version": "v1.0",
-    "thumb": "assets/img/thumb-openbor.svg"
+    "thumb": "assets/img/thumb-he-man.svg"
   },
   {
     "id": "ob-7735",
     "type": "openbor",
     "title": "OpenBOR – AltDünya Pack v1.0",
     "desc": "Kurulum rehberi + Archive.org indirme butonu. İlk başlangıç paketi.",
-    "tags": [
-      "openbor",
-      "engine",
-      "beatemup"
-    ],
+    "tags": ["openbor", "engine", "beatemup"],
     "url": "openbor/openbor-7735.html",
     "added": "2026-02-28",
     "version": "v1.0",
